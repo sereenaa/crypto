46cf02a4e19cc57e76163f695d25e230aa94bd8e
sereenaa <serenax8@gmail.com>
2024-08-11 22:55:26 +1000

    increase cpu and memory for ecs task

--- run_scripts.sh
+++ run_scripts.sh
@@ -6,9 +6,9 @@
 # Fetch the historical data with multi processing 
 python main.py historical 53294400 55057990 100 1 &
 python main.py historical 55057990 56819482 100 2 &
-python main.py historical 56819482 58580974 100 3 &
-python main.py historical 58580974 60342466 100 4 &
-python main.py historical 60342466 62103959 100 5 &
+python main.py historical 56821282 58580974 100 3 &
+python main.py historical 58588974 60342466 100 4 &
+python main.py historical 60342666 62103959 100 5 &
 
 # Wait for all background processes to finish
 wait
