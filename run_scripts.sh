1262aacb33a299d12c336fc8f23f7eaeb1a4ed70
sereenaa <serenax8@gmail.com>
2024-08-15 20:19:31 +1000

    update block numbers to latest

--- run_scripts.sh
+++ run_scripts.sh
@@ -4,11 +4,11 @@
 # python main.py default &
 
 # Fetch the historical data with multi processing 
-python main.py historical 53469300 55057990 100 1 &
-python main.py historical 55226490 56819482 100 2 &
-python main.py historical 56887982 58580974 100 3 &
-python main.py historical 58720574 60342466 100 4 &
-python main.py historical 60345566 62103959 100 5 &
+python main.py historical 53819100 55057990 100 1 &
+python main.py historical 55253290 56819482 100 2 &
+python main.py historical 57218682 58580974 100 3 &
+python main.py historical 59044474 60342466 100 4 &
+python main.py historical 60342467 62103959 100 5 &
 
 # Wait for all background processes to finish
 wait
