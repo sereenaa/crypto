23d4ebeaba4ed2f80c8907c3d76353dadfdde5f2
sereenaa <serenax8@gmail.com>
2024-08-16 22:40:03 +1000

    fix start blocks for runs

--- run_scripts.sh
+++ run_scripts.sh
@@ -4,11 +4,11 @@
 # python main.py default &
 
 # Fetch the historical data with multi processing 
-python main.py historical 53819100 55057990 100 1 &
-python main.py historical 55253290 56819482 100 2 &
-python main.py historical 57218682 58580974 100 3 &
-python main.py historical 59044474 60342466 100 4 &
-python main.py historical 60342467 62103959 100 5 &
+python main.py historical 54008340 55057990 100 1 &
+python main.py historical 55253389 56819482 100 2 &
+python main.py historical 57373582 58580974 100 3 &
+python main.py historical 59181874 60342466 100 4 &
+python main.py historical 60349967 62103959 100 5 &
 
 # Wait for all background processes to finish
 wait
